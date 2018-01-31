d6cc242803fa04729918c4ec5ceff4562f799c93
Karsten.A <arnoldedvpp@outlook.de>
2018-01-31 02:09:05 +0100

    some first code for the services

---
+++ StarfallNetworking/StarfallServices/StarfallPlayService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Starfall.StarfallServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "StarfallPlayService" in both code and config file together.
+    public class StarfallPlayService : IStarfallPlayService
+    {
+        public string GetServerStatus()
+        {
+            return "Starfall Service";
+        }
+    }
+}
---
+++ StarfallNetworking/StarfallWindowsServiceHost/StarfallServiceHost.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+
+namespace StarfallWindowsServiceHost
+{
+    public partial class StarfallServiceHost : ServiceBase
+    {
+        ServiceHost host;
+
+        public StarfallServiceHost()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            host = new ServiceHost(typeof(Starfall.StarfallServices.StarfallPlayService));
+            host.Open();
+        }
+
+        protected override void OnStop()
+        {
+            host.Close();
+        }
+    }
+}
